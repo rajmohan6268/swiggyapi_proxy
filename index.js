714dea3cd17bd8b377bf8f87e11a716a9832c1d3
rajmohan6268 <rajmohan6268@gmail.com>
2021-11-15 20:08:00 +0530

    fix err

--- index.js
+++ index.js
@@ -8,31 +8,30 @@ app.get("/api", (req, res) => {
   res.send("ok");
 });
 
+app.get("*", async (req, res) => {
+  console.log(req.originalUrl);
 
-// app.get("*", async (req, res) => {
-//   console.log(req.originalUrl);
+  const { body } = await got.get(
+    "https://www.swiggy.com/dapi" + `${req.originalUrl}`,
+    {
+      headers: {
+        __fetch_req__: "true",
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9,gu;q=0.8,ta;q=0.7",
+        "content-type": "application/json",
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+      },
 
-//   const { body } = await got.get(
-//     "https://www.swiggy.com/dapi" + `${req.originalUrl}`,
-//     {
-//       headers: {
-//         __fetch_req__: "true",
-//         accept: "*/*",
-//         "accept-language": "en-US,en;q=0.9,gu;q=0.8,ta;q=0.7",
-//         "content-type": "application/json",
-//         "user-agent":
-//           "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-//       },
-
-//       referrer: "https://www.swiggy.com/",
-//       referrerPolicy: "strict-origin-when-cross-origin",
-//       mode: "cors",
-//       responseType: "json",
-//     }
-//   )
-//   // console.log(body);
-//   res.json(body);
-// });
+      referrer: "https://www.swiggy.com/",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      mode: "cors",
+      responseType: "json",
+    }
+  );
+  // console.log(body);
+  res.json(body);
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
