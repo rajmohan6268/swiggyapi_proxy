c0dd7bf119fd02c82b1605ecb5b5547a5215a097
rajmohan6268 <rajmohan6268@gmail.com>
2021-11-15 20:06:18 +0530

    rm p.json

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const got = require("got");
+//const got = require("got");
 const cors = require("cors");
 const app = express();
-const port = 5000;
 app.use(cors());
-app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.json("ok");
+  res.send("ok");
 });
 
 
@@ -35,6 +33,7 @@ app.get("/api", (req, res) => {
 //   // console.log(body);
 //   res.json(body);
 // });
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
